c9fad97b563393681f0279a9f6774785b512208e
Bill Kable <bkable@gmail.com>
2020-07-06 21:32:47 -0500

    Add probe tests and pal-tracker failure endpoint
    # Conflicts:
    #	src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java

--- src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
@@ -3,13 +3,10 @@ package test.pivotal.pal.trackerapi;
 import com.jayway.jsonpath.DocumentContext;
 import io.pivotal.pal.tracker.PalTrackerApplication;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
+import org.springframework.http.*;
 
 import static com.jayway.jsonpath.JsonPath.parse;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,13 +22,58 @@ public class HealthApiTest {
     public void healthTest() {
         ResponseEntity<String> response = this.restTemplate.getForEntity("/actuator/health", String.class);
 
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext healthJson = parse(response.getBody());
+
+        assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.components.diskSpace.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.components.ping.status", String.class)).isEqualTo("UP");
+        assertThat(healthJson.read("$.components.backingService.status", String.class)).isEqualTo("UP");
+
+        ResponseEntity<Void> simulateFailurePostResponse = postSimulateFailure();
+
+        assertThat(simulateFailurePostResponse.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        response = this.restTemplate.getForEntity("/actuator/health", String.class);
+
+        healthJson = parse(response.getBody());
+
+        assertThat(healthJson.read("$.components.backingService.status", String.class)).isEqualTo("DOWN");
+    }
+
+    @Test
+    public void readinessProbeTest() {
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/actuator/health/readiness", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         DocumentContext healthJson = parse(response.getBody());
 
         assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
-        assertThat(healthJson.read("$.components.backingService.status", String.class)).isEqualTo("UP");
-        assertThat(healthJson.read("$.components.diskSpace.status", String.class)).isEqualTo("UP");
+    }
+
+    @Test
+    public void livenessProbeTest() {
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/actuator/health/liveness", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext healthJson = parse(response.getBody());
+
+        assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
+    }
+
+    private ResponseEntity<Void> postSimulateFailure() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("content-type", "application/json");
+
+        ResponseEntity<Void> simulateFailurePostResponse = this.restTemplate.exchange(
+                "/actuator/backingServiceFailure",
+                HttpMethod.POST,
+                new HttpEntity<>(httpHeaders),
+                Void.class
+        );
+        return simulateFailurePostResponse;
     }
 }
