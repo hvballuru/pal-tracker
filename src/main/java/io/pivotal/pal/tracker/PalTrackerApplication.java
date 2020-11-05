764ce209abd1c86a0f0dbc829906e070624ce195
ubuntu <a@b>
2020-11-05 21:47:34 +0000

    added file

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,12 +1,24 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
+    }
+    
+    @Bean
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new InMemoryTimeEntryRepository();
     }
 }
